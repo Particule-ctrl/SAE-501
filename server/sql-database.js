509e09f0de04caa69b3a87625d72a80107e552e1
Noe <lamarquenoe@gmail.com>
2024-10-04 19:42:34 +0200

    Added SQL database model to the server

---
+++ server/sql-database.js
@@ -0,0 +1,112 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
+                                process.env.DB_USER,
+                                process.env.DB_PASSWORD,
+                                {
+                                    host: process.env.DB_HOST || 'localhost',
+                                    port: process.env.DB_PORT || 5432,
+                                    dialect: 'postgres',
+                                    dialectOptions: {
+                                    ssl: process.env.DB_SSL == "true"
+                                    }
+                                }
+);
+
+const PMR = sequelize.define('PMR',{
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    birthdate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tel: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+
+const Accompagnateur = sequelize.define('Accompagnateur',{
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    birthdate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tel: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+
+const Agent = sequelize.define('Agent',{
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tel: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    site_id: {
+        type: Sequelize.INTEGER
+    }
+});
+
+const Site = sequelize.define('Site',{
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    geoCoord: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    country: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    geoSubDiv: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    city: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+});
+
+module.exports = {
+    sequelize: sequelize,
+    PMR: PMR,
+    Accompagnateur: Accompagnateur,
+    Agent: Agent,
+    Site: Site
+}
