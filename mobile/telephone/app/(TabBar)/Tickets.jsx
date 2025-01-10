72a0301bc504caac8aa2d6b7f05dab9f53e40114
mXn <legeardflavian291@gmail.com>
2025-01-10 11:45:44 +0100

    tickets update

--- mobile/telephone/app/(TabBar)/Tickets.jsx
+++ mobile/telephone/app/(TabBar)/Tickets.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import { Link } from 'expo-router';
-import { Tickets } from '../../components/home/Tickets/Tickets';
+import { View, StyleSheet } from 'react-native';
+import Tickets from '../../components/home/Tickets/Tickets';
 
-
-export default function Trafic() {
+export default function TrajetsListe() {
     return (
         <View style={styles.container}>
             <Tickets />
         </View>
-      )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#90ee90", 
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#90ee90',
     },
-    text: {
-      fontSize: 16,
-    },
-  });
-
-
+});
--- mobile/telephone/components/home/Tickets/Tickets.jsx
+++ mobile/telephone/components/home/Tickets/Tickets.jsx
@@ -1,63 +1,60 @@
-import { SafeAreaView, StyleSheet, Text, FlatList, Animated, View, Alert} from 'react-native';
-import React, { useState, useRef } from 'react';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, FlatList, View } from 'react-native';
 
 const DATA = [
-    {
-        id: '1',
-        depart: 'blablabla',
-        destination: 'blobloblo',
-        date: '01/01/2021',
-        points: '2',
-    },
-    {
-        id: '2',
-        depart: 'piririri',
-        destination: 'paraa',
-        date: '01/01/2021',
-        points: '8',
-    },
-    {
-        id: '3',
-        depart: 'dzdzdzdz',
-        destination: 'mrmrmr',
-        date: '09/02/2025',
-        points: '4',
-        etapes : ['dzdzdzdz', 'mrmrmr']
-    },
+    { id: '1', depart: 'Paris', destination: 'Tokyo', dateDepart: '01/01/2021', dateArriver: '03/01/2021', points: '2', ordre : ["bus", "plane"] },
+    { id: '2', depart: 'Paris', destination: 'Moscou', dateDepart: '08/01/2022',dateArriver: '12/02/2023',dateArriver: '03/01/2021', points: '3', ordre : ["train", "bus", "train"] },
+    { id: '3', depart: 'Paris', destination: 'Berlin', dateDepart: '09/02/2025',dateArriver: '19/02/2025', points: '4',  ordre : ["bus", "plane", "bus", "train"] },
 ];
 
 export default function Tickets() {
     return (
         <SafeAreaView style={styles.container}>
-            <FlatList data={DATA} 
-                renderItem={({ item }) => ( 
-                <View style={styles.item}>
-                    <Text style={styles.title}>{item.depart}</Text>
-                    <Text style={styles.title}>{item.destination}</Text>
-                    <Text style={styles.title}>{item.date}</Text>
-                    <Text style={styles.title}>{item.points}</Text>
-                </View>
-                )
-            }
-            keyExtractor={item => item.id}
+            <FlatList
+                data={DATA}
+                renderItem={({ item }) => (
+                    <View style={styles.item}>
+                        <View style={styles.lieu}>
+                            <Text style={styles.destination}>{item.depart}</Text>
+                            <Text style={styles.destination}>{item.destination}</Text>
+                        </View>
+                        <View style={styles.date}>
+                        <Text style={styles.title}>{item.dateDepart}</Text>
+                        <Text style={styles.title}>{item.dateArriver}</Text>
+                        </View>
+                        <Text style={styles.title}>{item.points}</Text>
+                    </View>
+                )}
+                keyExtractor={(item) => item.id}
             />
-
         </SafeAreaView>
-    )
-
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            marginTop: 20,
-        },
-        item: {
-            backgroundColor: '#f9c2ff',
-            padding: 20,
-            marginVertical: 8,
-            marginHorizontal: 16,
-        },
-        title: {
-            fontSize: 32,
-        },
-    });
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0,
+        marginTop: 20,
+        width: '100%',
+    },
+    item: {
+        backgroundColor: '#f4f5f9',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 10,
+    },
+
+    destination: {
+        fontSize: 20,
+    },
+    lieu: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    date: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    
+});
